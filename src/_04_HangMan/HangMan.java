09f03c59daa03ef189b178a67194dc11e82bd6a8
acutiepieg <league@WTS-IM-6.attlocal.net>
2019-04-29 16:59:09 -0700

    HangMan(ip)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,24 +1,36 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class HangMan {
+public class HangMan implements KeyListener {
 	String numWords1;
 	int numWords;
+	String hangManWord;
 
 	Stack<String> words = new Stack<String>();
 	JLabel label;
+	JFrame frame;
 
+	public static void main(String[] args) {
+		HangMan h = new HangMan();
+	}
+	
 	public HangMan() {
-		JFrame frame = new JFrame();
+		startGame();
+		frame = new JFrame();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		label = new JLabel();
 		frame.add(label);
+		label.setText("");
+		frame.addKeyListener(this);
+		addWord();
 	}
 
 	public void startGame() {
@@ -27,8 +39,40 @@ public class HangMan {
 		for(int i = 0; i < numWords; i ++) {
 			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		}
-	HangMan h = new HangMan();
 	}
 	
+	public void addWord() {
+		hangManWord = words.pop();
+		for(int i = 0; i < hangManWord.length(); i ++) {
+			label.setText(label.getText() + " _");
+		}
+		label.setText(label.getText() + " Lives: ");
+		frame.pack();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(hangManWord.contains(e.getKeyChar()) == true) {
+			
+		}
+		
+		
+		if(hangManWord.contains("_") == false) {
+			addWord();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
