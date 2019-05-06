2424dd8f7554ab33a426f86628ca8a35a67e03b6
zakbamford <league@WTS-IM-6.attlocal.net>
2019-05-06 16:57:52 -0700

    finish lives on hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -12,6 +12,8 @@ public class HangMan implements KeyListener {
 	String numWords1;
 	int numWords;
 	String hangManWord;
+	String wordShown = " ";
+	int lives = 10;
 
 	Stack<String> words = new Stack<String>();
 	JLabel label;
@@ -20,7 +22,7 @@ public class HangMan implements KeyListener {
 	public static void main(String[] args) {
 		HangMan h = new HangMan();
 	}
-	
+
 	public HangMan() {
 		startGame();
 		frame = new JFrame();
@@ -36,43 +38,60 @@ public class HangMan implements KeyListener {
 	public void startGame() {
 		numWords1 = JOptionPane.showInputDialog("How many words would you like to play?");
 		numWords = Integer.parseInt(numWords1);
-		for(int i = 0; i < numWords; i ++) {
+		for (int i = 0; i < numWords; i++) {
 			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		}
 	}
-	
+
 	public void addWord() {
-		hangManWord = words.pop();
-		for(int i = 0; i < hangManWord.length(); i ++) {
-			label.setText(label.getText() + " _");
+		hangManWord = words.peek();
+		System.out.println(hangManWord);
+		wordShown = "";
+		for (int i = 0; i < hangManWord.length(); i++) {
+			wordShown = wordShown + "_";
 		}
-		label.setText(label.getText() + " Lives: ");
+		label.setText(wordShown + " Lives: " + lives);
 		frame.pack();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(hangManWord.contains(e.getKeyChar()) == true) {
-			
+		char character = e.getKeyChar();
+		for (int i = 0; i < hangManWord.length(); i++) {
+			if (hangManWord.charAt(i) == character) {
+				wordShown = wordShown.substring(0, i) + character + wordShown.substring(i + 1, wordShown.length());
+				label.setText(wordShown + " Lives: " + lives);
+			} else {
+				lives = lives - 1;
+			}
 		}
-		
-		
-		if(hangManWord.contains("_") == false) {
-			addWord();
+
+		if (wordShown.contains("_") == false) {
+			words.pop();
+			if (words.size() == 0) {
+				JOptionPane.showMessageDialog(null, "You have no more words left");
+			}
+			if (lives == 0) {
+				JOptionPane.showMessageDialog(null, "You have no lives left");
+			} else {
+				label.setText("");
+				lives = 10;
+				addWord();
+			}
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
